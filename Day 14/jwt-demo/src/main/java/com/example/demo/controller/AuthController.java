d8f2af8712e0ab09109817a5c066b6cf7077679c
Ramprajeeth <166988710+Ramprajeeth@users.noreply.github.com>
2026-04-01 13:22:08 +0530

    Add files via upload

---
+++ Day 14/jwt-demo/src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.dto.LoginResponse;
+import com.example.demo.security.JwtUtil;
+import com.example.demo.service.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+	@Autowired
+	private AuthService authService;
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+		boolean isValidUser = authService.authenticate(request.getUsername(), request.getPassword());
+		if (!isValidUser) {
+			return ResponseEntity.status(401).body("Invalid username or password");
+		}
+		String token = jwtUtil.generateToken(request.getUsername());
+		return ResponseEntity.ok(new LoginResponse(token));
+	}
+}
